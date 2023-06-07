834bc378c182a42b9f3ca043ab597e0e601872d5
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-06-07 14:15:46 +0000

    Deploying to gh-pages from @ DmytroZhdanov/project-DP_UNITED@947d0857ccfe00b07d341ff9ef6411e0000b03bf 🚀

---
+++ index.47b9e21a.js
@@ -0,0 +1,2 @@
+document.querySelector(".trends")?mainPage.classList.add("current"):document.querySelector(".catalogue")?cataloguePage.classList.add("current"):document.querySelector(".library")&&libraryPage.classList.add("current");
+//# sourceMappingURL=index.47b9e21a.js.map
---
+++ index.d65d4127.js
@@ -0,0 +1,2 @@
+document.querySelector(".trends")?mainPage.classList.add("current"):document.querySelector(".catalogue")?cataloguePage.classList.add("current"):document.querySelector(".library")&&libraryPage.classList.add("current");
+//# sourceMappingURL=index.d65d4127.js.map
