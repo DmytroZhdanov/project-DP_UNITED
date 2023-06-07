fa97893e6d2ceed536e58ba00b812bc8064bbe90
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-06-07 17:44:55 +0200

    Exports API data

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://developers.themoviedb.org/3';
-const API_KEY = '55796f17387279855c720b27f49c25f1';
-const TOKEN =
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const API_KEY = '55796f17387279855c720b27f49c25f1';
+export const TOKEN =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTc5NmYxNzM4NzI3OTg1NWM3MjBiMjdmNDljMjVmMSIsInN1YiI6IjY0N2Y3MDYzNzFmZmRmMDEwYzg3YWIzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9AzF80qawQzmYwfptJjDK5gSiKy9M6ZEOpq2I4PFWtg';
