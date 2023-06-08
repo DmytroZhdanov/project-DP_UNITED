a0955ca94124d6ce1317ab767fc5e8daad5301ff
Rinya7 <v.dmitriev0712@gmail.com>
2023-06-08 17:56:39 +0200

    library
    подготовли страницу для рендера с локал сторедж.

--- src/js/list-library.js
+++ src/js/list-library.js
@@ -1,12 +1,30 @@
 // Library Page - Library list section
 
+//=========================Достаем данные с Локал сторедж и рисуем блок с фильмами========================
+
+function outFromLocalStorage() {
+  const checkData = JSON.parse(localStorage.getItem(STRORAGE_LIST_LIBRARY));
+  if (checkData) {
+    createPage(checkData);
+    //  dataForm.email = checkData.email;
+    //  dataForm.message = checkData.message;
+    //  refs.inputEmail.value = checkData.email;
+    //  refs.textArea.value = checkData.message;
+  } else {
+    //=============если фильмов нет рисуем то что по умолчанию ==================
+    parceHtmlNoFavoriteFilm();
+  }
+}
+
+//==================================END)======================================
+
 //Прописал временно прием данных а АПИ сервера для рендера страницы===================================
 const axios = require('axios').default;
 const KEY =
   'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzNkZjVhNDE2N2JjNTk5ZWEwMWNjNmQyNGNjNzVhZCIsInN1YiI6IjY0N2VmMjg1OTM4MjhlMDBkY2RlNWNhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kqCYz0SAB1p5Vs4x_lTPXyMV57XvzeJMma8vmsX5SKs';
 
 const refs = {
-  contentForPictures: document.querySelector('.gallery'),
+  contentForPictures: document.querySelector('.container'),
 };
 
 const options = {
@@ -31,6 +49,7 @@ async function getApiSearch() {
 }
 //============================END (временно прописал выше с АПИ прием данных )===========================
 //запускаем функцию рендера страницы (нужно переделать прием данных с локал сторидж)
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -40,11 +59,22 @@ if (a > 5) {
   getApiSearch(URL).then(data => {
     console.log(data.results);
     createPage(data.results);
+    //refs.contentForPictures.insertAdjacentHTML(
+    //  'beforebegin',
+    //  `
+    //  <div class="dropdown">
+    //  <button class="dropbtn">Dropdown</button>
+    //  <div class="dropdown-content">
+    //    <a href="#">Link 1</a>
+    //    <a href="#">Link 2</a>
+    //    <a href="#">Link 3</a>
+    //  </div>
+    //</div>
+    //`
+    //);
   });
 } else {
-  refs.contentForPictures.innerHTML = `<li>OOPS...</li>
-   <li>We are very sorry!</li>
-   <li>You don’t have any movies at your library.</li>`;
+  parceHtmlNoFavoriteFilm();
 }
 
 function createPage(arr) {
@@ -52,11 +82,21 @@ function createPage(arr) {
     .map(
       ({ poster_path, backdrop_path }) =>
         (refs.contentForPictures.innerHTML = `
-           <a class="gallery__link" href=https://api.themoviedb.org/3/authentication${backdrop_path}>
-             <div class="photo-card">
-             <img src="https://image.tmdb.org/t/p//w500${poster_path}" alt="title" loading="lazy" />
-
-           </div></a>`)
+        
+        <a class="gallery__link" href=https://api.themoviedb.org/3/authentication${backdrop_path}>
+            <img class="img-poster" src="https://image.tmdb.org/t/p//w500${poster_path}" alt="title" width="280" heigth="406" loading="lazy" />
+        </a>
+        `)
     )
     .join(''));
+}
+
+function parceHtmlNoFavoriteFilm() {
+  refs.contentForPictures.innerHTML = `
+  <ul class="oops">
+    <li>OOPS...</li>
+    <li>We are very sorry!</li>
+    <li>You don’t have any movies at your library.</li>
+    </ul>
+    `;
 }
