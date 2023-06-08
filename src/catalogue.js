b6af0b0f4af65c553c8eefce9cbf9250129fd398
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-06-08 10:49:28 +0300

    upd 2

--- src/catalogue.js
+++ src/catalogue.js
@@ -8,9 +8,9 @@ import './js/modal-trailer';
 import './js/modal-movie-details';
 
 
-const headerCurrent = document.querySelector('.header-home')
-const headerCatCurrent = document.querySelector('.header-catalogue')
-const headerLibCurrent = document.querySelector('.header-library')
+const headerCurrent = document.querySelector('.js-header-home')
+const headerCatCurrent = document.querySelector('.js-header-catalogue')
+const headerLibCurrent = document.querySelector('.js-header-library')
 headerCatCurrent.classList.add('current')
 headerLibCurrent.classList.remove('current')
 headerCurrent.classList.remove('current')
--- src/library.js
+++ src/library.js
@@ -6,9 +6,9 @@ import './js/list-library';
 import './js/footer';
 
 
-const headerCurrent = document.querySelector('.header-home')
-const headerCatCurrent = document.querySelector('.header-catalogue')
-const headerLibCurrent = document.querySelector('.header-library')
+const headerCurrent = document.querySelector('.js-header-home')
+const headerCatCurrent = document.querySelector('.js-header-catalogue')
+const headerLibCurrent = document.querySelector('.js-header-library')
 headerLibCurrent.classList.add('current')
 headerCatCurrent.classList.remove('current')
 headerCurrent.classList.remove('current')
