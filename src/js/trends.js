39c2577268cdafeb245f3e187ad90bd8f2a175ba
Dmytro Zhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-06-09 22:30:47 +0200

    Update trends.js

--- src/js/trends.js
+++ src/js/trends.js
@@ -1,16 +1,27 @@
 // Weekly trends js
 import { fetchWeekTrends } from "./functions/movieApiService";
+import { generateRatingStarsMarkup } from './functions/generateRatingStarsMarkup';
+import { generateMovieCardsMarkup } from './functions/generateMovieCardsMarkup';
 
-const trendsContainer = document.querySelector('[data-list]');
+const weeklyTrendsList = document.querySelector('[data-weekly-trends-list]');
 
-const weekTrends = fetchWeekTrends();
-weekTrends.then(async(value) => {
-    const trendsData = await value.results.slice(0,3)
-    trendsData.forEach(element => {
-        let markup = `<li><a class="trend-film-link" href=""><img class="trend-film-thumb" width="500px" height="281px" src="https://image.tmdb.org/t/p//w500${element.backdrop_path}"/>${element.title} Mark: ${element.vote_average.toFixed(1)}</a></li>`;
-        trendsContainer.innerHTML = markup;
-    });
+renderWeeklyTrendsItems()
+
+async function renderWeeklyTrendsItems() {
+  const response = await fetchWeekTrends();
+  const moviesToRender = response.results.slice(0,3)
+  const weeklyTrendsMarkup = generateMovieCardsMarkup(moviesToRender)
+  weeklyTrendsList.innerHTML = weeklyTrendsMarkup
+}
+
+// const weekTrends = fetchWeekTrends();
+// weekTrends.then(async(value) => {
+//     const trendsData = await value.results.slice(0,3)
+//     trendsData.forEach(element => {
+//         let markup = `<li><a class="trend-film-link" href=""><img class="trend-film-thumb" width="500px" height="281px" src="https://image.tmdb.org/t/p//w500${element.backdrop_path}"/>${element.title} Mark: ${element.vote_average.toFixed(1)}</a></li>`;
+//         trendsContainer.innerHTML = markup;
+//     });
     
-})
+// })
 
 
