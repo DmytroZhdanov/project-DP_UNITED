d988bd819923f953216e4dc19dc2dfd5c5bcfdc6
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-06-09 18:45:00 +0300

    trends.css+trends.js
    чекаю твоєї функції, це вже можна заливати

--- src/js/trends.js
+++ src/js/trends.js
@@ -1,14 +1,14 @@
 // Weekly trends js
 import { fetchWeekTrends } from "./functions/movieApiService";
 
-const trendsContainer = document.querySelector('.weekly-trends-list');
+const trendsContainer = document.querySelector('[data-list]');
 
 const weekTrends = fetchWeekTrends();
-weekTrends.then((value) => {
-    const trendsData = value.results.slice(0,3)
+weekTrends.then(async(value) => {
+    const trendsData = await value.results.slice(0,3)
     trendsData.forEach(element => {
         let markup = `<li><a class="trend-film-link" href=""><img class="trend-film-thumb" width="500px" height="281px" src="https://image.tmdb.org/t/p//w500${element.backdrop_path}"/>${element.title} Mark: ${element.vote_average.toFixed(1)}</a></li>`;
-        trendsContainer.insertAdjacentHTML("afterbegin",markup);
+        trendsContainer.innerHTML = markup;
     });
     
 })
