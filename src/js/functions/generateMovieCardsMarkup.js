e103d0ca7e8885d0fa2b7a7e85faf28838b505c4
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-06-09 22:13:29 +0200

    Adds function for generating movie cards markup

---
+++ src/js/functions/generateMovieCardsMarkup.js
@@ -0,0 +1,37 @@
+import { generateRatingStarsMarkup } from './generateRatingStarsMarkup';
+/**
+ * 
+ * @param {Array} arr 
+ * @returns {String} markup for movie cards. You should insert resul inside <ul> by innerHTML
+ */
+function generateMovieCardsMarkup(arr) {
+  return arr.map(
+    ({ genre_ids, id, poster_path, release_date, title, vote_average }) => {
+      const year = release_date.slice(0, 4);
+      const posterPath = poster_path
+        ? `https://image.tmdb.org/t/p/w500${poster_path}`
+        : './images/image-not-found.jpg';
+      const ratingStarsMurkup = generateRatingStarsMarkup(vote_average); 
+      // Temporarily
+      const genres = genre_ids.join(' ');
+      // Temporarily
+      return `<li class="cards-item" data-movie-card-id="${id}">
+      <img
+        src="${posterPath}"
+        alt="Poster of the movie"
+        class="movie-card-poster"
+      />
+      <div class="cards-content">
+        <div class="cards-text-content">
+          <h3 class="cards-title">${title}</h3>
+          <p class="cards-text">${genres} | ${year}</p>
+        </div>
+        <div class="rating-stars-wrapper">${ratingStarsMurkup}
+        </div>
+      </div>
+    </li>`;
+    }
+  ).join('');
+}
+
+export { generateMovieCardsMarkup };
--- src/library.js
+++ src/library.js
@@ -5,9 +5,9 @@ import './js/hero';
 import './js/list-library';
 import './js/footer';
 
-const headerCurrent = document.querySelector('.js-header-home')
-const headerCatCurrent = document.querySelector('.js-header-catalogue')
-const headerLibCurrent = document.querySelector('.js-header-library')
-headerLibCurrent.classList.add('current')
-headerCatCurrent.classList.remove('current')
-headerCurrent.classList.remove('current')
+const headerCurrent = document.querySelector('[data-header-home]');
+const headerCatCurrent = document.querySelector('[data-header-catalogue]');
+const headerLibCurrent = document.querySelector('[data-header-library]');
+headerLibCurrent.classList.add('js-current')
+headerCatCurrent.classList.remove('js-current')
+headerCurrent.classList.remove('js-current')
