2eadcb0bbeb3fb6f4b7ed3dfb32efe02a1fc1b12
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-06-09 15:38:08 +0300

    upd

--- src/js/trends.js
+++ src/js/trends.js
@@ -1,18 +1,15 @@
 // Weekly trends js
 import { fetchWeekTrends } from "./functions/movieApiService";
 
-const trendsContainer = document.querySelector('.weekly-trends');
+const trendsContainer = document.querySelector('.weekly-trends-list');
 
 const weekTrends = fetchWeekTrends();
-const filmTrends = [];
 weekTrends.then((value) => {
-    const trendsData = value.results.map(film => {
-       filmTrends.push(film)
-    })
-    for (let i=0;i<=2;i+=1){
-        let markup = `<a class="trend-film-link" href=""><img class="trend-film-thumb" width="500px" height="281px" href="https://image.tmdb.org/t/p//w500${filmTrends[i].backdrop_path}"/>${filmTrends[i].title} Mark: ${filmTrends[i].vote_average.toFixed(1)}</a>`;
+    const trendsData = value.results.slice(0,3)
+    trendsData.forEach(element => {
+        let markup = `<li><a class="trend-film-link" href=""><img class="trend-film-thumb" width="500px" height="281px" src="https://image.tmdb.org/t/p//w500${element.backdrop_path}"/>${element.title} Mark: ${element.vote_average.toFixed(1)}</a></li>`;
         trendsContainer.insertAdjacentHTML("afterbegin",markup);
-    }
+    });
     
 })
 
