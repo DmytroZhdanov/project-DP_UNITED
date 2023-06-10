832d83af842a00ce5a3df53a069971fb01574b6f
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-06-10 11:16:41 +0200

    Refactors weekly trends styles and js

--- src/js/trends.js
+++ src/js/trends.js
@@ -1,21 +1,16 @@
 // Weekly trends js
-import { fetchWeekTrends } from "./functions/movieApiService";
-// import { generateRatingStarsMarkup } from './functions/generateRatingStarsMarkup';
+import { fetchWeekTrends } from './functions/movieApiService';
 import { generateMovieCardsMarkup } from './functions/generateMovieCardsMarkup';
 
 const weeklyTrendsList = document.querySelector('[data-weekly-trends-list]');
 
-renderWeeklyTrendsItems()
-
 const moviesNumber = window.innerWidth < 768 ? 1 : 3;
+
+renderWeeklyTrendsItems();
 
 async function renderWeeklyTrendsItems() {
   const response = await fetchWeekTrends();
-  const moviesToRender = response.results.slice(0,moviesNumber)
-  const weeklyTrendsMarkup = generateMovieCardsMarkup(moviesToRender)
-  weeklyTrendsList.innerHTML = weeklyTrendsMarkup
+  const moviesToRender = response.results.slice(0, moviesNumber);
+  const weeklyTrendsMarkup = generateMovieCardsMarkup(moviesToRender);
+  weeklyTrendsList.innerHTML = weeklyTrendsMarkup;
 }
-
-console.log(window.innerWidth)
-
-
