86a2063f841fdd107fe46a028c100c9ba5f64f15
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-06-10 10:36:24 +0300

    допрацьована секція трендів
    перевір як воно на проді відображатися буде(залежність кількості трендових фільмів від ширини вьюпорту)

--- src/js/trends.js
+++ src/js/trends.js
@@ -7,21 +7,15 @@ const weeklyTrendsList = document.querySelector('[data-weekly-trends-list]');
 
 renderWeeklyTrendsItems()
 
+const moviesNumber = window.innerWidth < 768 ? 1 : 3;
+
 async function renderWeeklyTrendsItems() {
   const response = await fetchWeekTrends();
-  const moviesToRender = response.results.slice(0,3)
+  const moviesToRender = response.results.slice(0,moviesNumber)
   const weeklyTrendsMarkup = generateMovieCardsMarkup(moviesToRender)
   weeklyTrendsList.innerHTML = weeklyTrendsMarkup
 }
 
-// const weekTrends = fetchWeekTrends();
-// weekTrends.then(async(value) => {
-//     const trendsData = await value.results.slice(0,3)
-//     trendsData.forEach(element => {
-//         let markup = `<li><a class="trend-film-link" href=""><img class="trend-film-thumb" width="500px" height="281px" src="https://image.tmdb.org/t/p//w500${element.backdrop_path}"/>${element.title} Mark: ${element.vote_average.toFixed(1)}</a></li>`;
-//         trendsContainer.innerHTML = markup;
-//     });
-    
-// })
+console.log(window.innerWidth)
 
 
