d7110da54479e0fd827fe725549bceff0b53e709
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-06-10 15:12:17 +0200

    Adds function to transform genres_ids into genres

--- src/index.js
+++ src/index.js
@@ -8,10 +8,14 @@ import './js/footer';
 import './js/modal-trailer';
 import './js/modal-movie-details';
 
-const headerCurrent = document.querySelector('[data-header-home]')
+const headerCurrent = document.querySelector('[data-header-home]');
 const headerCatCurrent = document.querySelector('[data-header-catalogue]');
 const headerLibCurrent = document.querySelector('[data-header-library]');
-headerCurrent.classList.add('js-current')
-headerLibCurrent.classList.remove('js-current')
-headerCatCurrent.classList.remove('js-current')
+headerCurrent.classList.add('js-current');
+headerLibCurrent.classList.remove('js-current');
+headerCatCurrent.classList.remove('js-current');
 
+import { getGenresByGenresId } from './js/functions/getGenresByGenresId';
+
+const genres = getGenresByGenresId([28, 12, 16]);
+console.log(genres);
