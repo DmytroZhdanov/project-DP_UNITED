5c7389ea743060c6e8decdd59f5743461ea26ae3
Ksenia <aliksukerch@gmail.com>
2023-06-10 16:05:42 +0300

    fixed html\css

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,1 +1,18 @@
 // Footer js
+
+document.querySelector('.developers-info').addEventListener('click', function() {
+    document.getElementById('modal-window-footer').style.display = 'block';
+});
+
+document.getElementById('modal-window-footer').addEventListener('click', function(e) {
+    if (e.target === this) {
+        this.style.display = 'none';
+    }
+});
+
+document.addEventListener('click', function(e) {
+    const modal = document.getElementById('modal-window-footer');
+    if (!modal.contains(e.target) && e.target !== document.querySelector('.developers-info')) {
+        modal.style.display = 'none';
+    }
+});
