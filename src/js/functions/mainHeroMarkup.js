221809cb5e22d1c5203f1df719d055dd6c8905ee
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-06-10 07:56:40 +0200

    Adds solution for hero buttons

--- src/js/functions/mainHeroMarkup.js
+++ src/js/functions/mainHeroMarkup.js
@@ -24,14 +24,33 @@ function createHomeHeroMarkup({
       <p class="hero-main-film-overview">
         ${overview}
       </p>
-      <div class="hero-main-buttons-container">
-        <button type="button" data-movie-id="${id}" class="btn watch-trailer-btn btn-filled">
-          Watch trailer
-        </button>
-        <button type="button" data-movie-id="${id}" class="more-details-btn">More details</button>
-      </div>
+      <ul class="hero-buttons-list">
+        <li class="hero-btn-item">
+          <button type="button" data-movie-id="${id}" class="btn btn-filled">
+            Watch trailer
+          </button>
+        </li>
+        <li class="hero-btn-item">
+          <button type="button" data-movie-id="${id}" class="btn btn-empty btn-black">More details</button>
+        </li>
+      </ul>
     </div>
   </div>`;
 }
 
 export { createHomeHeroMarkup };
+
+  // Розмітку кнопок змінив для тесту. Твою, оригінальну залишаю нижче
+  
+{/* <div class="hero-main-buttons-container">
+  <button
+    type="button"
+    data-movie-id="${id}"
+    class="btn watch-trailer-btn btn-filled"
+  >
+    Watch trailer
+  </button>
+  <button type="button" data-movie-id="${id}" class="more-details-btn">
+    More details
+  </button>
+</div>; */}
