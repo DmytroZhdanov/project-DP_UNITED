2982ef763da997b39661aee376dab6f797fc6ab2
Rinya7 <v.dmitriev0712@gmail.com>
2023-06-12 18:22:44 +0200

    button

--- src/js/list-library.js
+++ src/js/list-library.js
@@ -4,21 +4,43 @@ import { generateMovieCardsMarkup } from './functions/generateMovieCardsMarkup';
 //=========================Достаем данные с Локал сторедж и рисуем блок с фильмами========================
 
 const refs = {
-  libraryMovieCard: document.querySelector('[data-library-list]'),
+  libraryMovieList: document.querySelector('[data-library-list]'),
   libraryOppsText: document.querySelector('[data-library-oops-text]'),
 };
 
 const filmsInLibrary = JSON.parse(localStorage.getItem('library'));
 
 if (filmsInLibrary) {
-  refs.libraryMovieCard.innerHTML = generateMovieCardsMarkup(filmsInLibrary);
-  refs.libraryMovieCard.classList.add('movie-cards-list-css');
+  if (filmsInLibrary.length) {
+    refs.libraryMovieList.innerHTML = generateMovieCardsMarkup(filmsInLibrary);
+    refs.libraryMovieList.insertAdjacentHTML(
+      'afterend',
+      `
+  <div class="btn-list-search">
+      <button type="button" class="btn btn-filled" data-load-more-btn>
+      Load more
+      </button>
+    </div>`
+    );
+
+    refs.libraryMovieList.classList.add('movie-cards-list-css');
+  } else {
+    libraryOppsTextRender();
+  }
 } else {
+  libraryOppsTextRender();
+}
+
+function libraryOppsTextRender() {
   refs.libraryOppsText.innerHTML = `
       <p>OOPS...<br>
       We are very sorry!<br>
-      You don’t have any movies at your library.</ps=>
-
+      You don’t have any movies at your library.</p>
+        <div class="btn-list-search">
+        <a  href="./catalogue.html" class="btn btn-filled">
+        Search movie
+        </a>
+        </div>
       `;
   refs.libraryOppsText.classList.add('library-ops-text-css');
 }
