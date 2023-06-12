35e506e06de1ec2228a2fb0f074a15312532ef9b
LexMorrison <borrlex11@gmail.com>
2023-06-12 22:10:35 +0200

    fixed structure of Upcoming Section

---
+++ src/js/functions/generateUpcomingSectionMarkup.js
@@ -0,0 +1,96 @@
+import { getGenresByGenresId } from './getGenresByGenresId';
+
+function generateUpcomingSectionMarkup({
+  backdrop_path,
+  title,
+  genre_ids,
+  overview,
+  popularity,
+  poster_path,
+  release_date,
+  vote_average,
+  vote_count,
+}) {
+  const genresNameArr = getGenresByGenresId(genre_ids);
+  const genres = genresNameArr.join(', ');
+
+  return ` <picture>
+      <source
+        srcset="https://image.tmdb.org/t/p/original/${backdrop_path}"
+        media="(min-width:1280px)"
+      />
+      <source
+        srcset="https://image.tmdb.org/t/p/original/${backdrop_path}"
+        media="(min-width:768px)"
+      />
+      <source
+        srcset="https://image.tmdb.org/t/p/original/${poster_path}"
+        media="(min-width:320px)"
+      />
+      <img
+        class="upcoming-movie-img"
+        src="https://image.tmdb.org/t/p/original/${poster_path}"
+        alt="${title}"
+      />
+    </picture>
+    
+    <div class="upcoming-column">
+      <h2 class="upcoming-headline">${title}</h2>
+      <div class="upcoming-wrap">
+        <div class="upcoming-inner-wrap">
+          <div class="upcoming-wrapper upcoming-wrapper-gap">
+            <ul class="upcoming-list">
+              <li class="upcoming-item-data">
+                <p class="upcoming-subject">Release date</p>
+              </li>
+              <li class="upcoming-item-data-num">
+                <p class="upcoming-date">${release_date}</p>
+              </li>
+            </ul>
+    
+            <ul class="upcoming-list">
+              <li class="upcoming-item-vote">
+                <p class="upcoming-subject">Vote / Votes</p>
+              </li>
+              <li class="upcoming-item-vote-num">
+                <p class="upcoming-vote">
+                  <span class="upcoming-inner-box">${vote_average}</span>
+                  <span class="upcoming-inner-color">/</span>
+                  <span class="upcoming-inner-box-votes">${vote_count}</span>
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div class="upcoming-wrapper">
+            <ul class="upcoming-list">
+              <li class="upcoming-item-pop">
+                <p class="upcoming-subject">Popularity</p>
+              </li>
+              <li class="upcoming-item-pop-num">
+                <p class="upcoming-famous">${popularity.toFixed(1)}</p>
+              </li>
+            </ul>
+    
+            <ul class="upcoming-list">
+              <li class="upcoming-item-genre">
+                <p class="upcoming-subject">Genre</p>
+              </li>
+              <li class="upcoming-item-genre-num">
+                <p class="upcoming-janres">${genres}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+    
+        <h3 class="upcoming-caption">About</h3>
+        <p class="upcoming-description">
+          ${overview}
+        </p>
+        <button class="btn btn-filled" type="button">
+          Add to my library
+        </button>
+      </div>
+    </div>`;
+}
+
+export { generateUpcomingSectionMarkup };
--- src/js/upcoming.js
+++ src/js/upcoming.js
@@ -1,10 +1,7 @@
 // Upcoming this month js
-import {
-  fetchUpcomingThisMonth,
-  fetchGenresList,
-} from './functions/movieApiService';
+import { fetchUpcomingThisMonth } from './functions/movieApiService';
 
-const upcomingConatainer = document.querySelector('.upcoming-flex');
+import { generateUpcomingSectionMarkup } from './functions/generateUpcomingSectionMarkup';
 
 gettingMovie();
 async function gettingMovie() {
@@ -12,108 +9,6 @@ async function gettingMovie() {
   const uncomingArray = data.results;
   const randomMovie =
     uncomingArray[Math.floor(Math.random() * uncomingArray.length)];
-
-  upcomingConatainer.innerHTML = await renderingUpcomingMovie(randomMovie);
-}
-
-async function renderingUpcomingMovie({
-  backdrop_path,
-  title,
-  genre_ids,
-  overview,
-  popularity,
-  poster_path,
-  release_date,
-  vote_average,
-  vote_count,
-}) {
-  const genres = await fetchGenres(genre_ids);
-  return ` <picture>
-  <source
-    srcset="https://image.tmdb.org/t/p/original/${backdrop_path}"
-    media="(min-width:1280px)"
-  />
-  <source
-    srcset="https://image.tmdb.org/t/p/original/${backdrop_path}"
-    media="(min-width:768px)"
-  />
-  <source
-    srcset="https://image.tmdb.org/t/p/original/${poster_path}"
-    media="(min-width:320px)"
-  />
-  <img
-    class="upcoming-movie-img"
-    src="https://image.tmdb.org/t/p/original/${poster_path}"
-    alt="${title}"
-  />
-</picture>
-
-<div class="upcoming-column">
-  <h2 class="upcoming-headline">${title}</h2>
-  <div class="upcoming-wrap">
-    <div class="upcoming-inner-wrap">
-      <div class="upcoming-wrapper upcoming-wrapper-gap">
-        <ul class="upcoming-list">
-          <li class="upcoming-item-data">
-            <p class="upcoming-subject">Release date</p>
-          </li>
-          <li class="upcoming-item-data-num">
-            <p class="upcoming-date">${release_date}</p>
-          </li>
-        </ul>
-
-        <ul class="upcoming-list">
-          <li class="upcoming-item-vote">
-            <p class="upcoming-subject">Vote / Votes</p>
-          </li>
-          <li class="upcoming-item-vote-num">
-            <p class="upcoming-vote">
-              <span class="upcoming-inner-box">${vote_average}</span>
-              <span class="upcoming-inner-color">/</span>
-              <span class="upcoming-inner-box-votes">${vote_count}</span>
-            </p>
-          </li>
-        </ul>
-      </div>
-      <div class="upcoming-wrapper">
-        <ul class="upcoming-list">
-          <li class="upcoming-item-pop">
-            <p class="upcoming-subject">Popularity</p>
-          </li>
-          <li class="upcoming-item-pop-num">
-            <p class="upcoming-famous">${popularity.toFixed(1)}</p>
-          </li>
-        </ul>
-
-        <ul class="upcoming-list">
-          <li class="upcoming-item-genre">
-            <p class="upcoming-subject">Genre</p>
-          </li>
-          <li class="upcoming-item-genre-num">
-            <p class="upcoming-janres">${genres}</p>
-          </li>
-        </ul>
-      </div>
-    </div>
-
-    <h3 class="upcoming-caption">About</h3>
-    <p class="upcoming-description">
-      ${overview}
-    </p>
-    <button class="btn btn-filled" type="button">
-      Add to my library
-    </button>
-  </div>
-</div>`;
-}
-
-async function fetchGenres(ids) {
-  const genresData = await fetchGenresList();
-
-  const genresArray = genresData.genres;
-  const genresName = ids.map(id => {
-    const gener = genresArray.find(gener => gener.id === id);
-    return gener.name;
-  });
-  return genresName.join(', ');
+  const upcomingConatainer = document.querySelector('.upcoming-flex');
+  upcomingConatainer.innerHTML = generateUpcomingSectionMarkup(randomMovie);
 }
