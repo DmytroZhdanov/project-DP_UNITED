8a8ab06dfbb3ed489b048d5a7a6d4bd514590f71
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-06-13 11:39:10 +0200

    Ammends connection modal trailer

--- src/js/functions/openModalTrailer.js
+++ src/js/functions/openModalTrailer.js
@@ -5,17 +5,17 @@ import { modalClose } from './modalClose';
 
 async function openModalTrailer(movieId) {
   const response = await fetchMovieVideosById(movieId);
-  const trailerKey = response.results[0].key;
+  const trailerKey = response.results[0].ke;
   const modaltrailerContainer = document.querySelector('[data-modal-trailer]');
 
-  modaltrailerContainer.innerHTML = generateModalTrailerMarkup(trailerKey);
-  modaltrailerContainer.classList.remove('is-hidden');
-  modaltrailerContainer.addEventListener('click', onBackdropClick);
-
-  if (!trailerKey) {
+  if (trailerKey) {
+    modaltrailerContainer.innerHTML = generateModalTrailerMarkup(trailerKey);
+  } else {
     const modalCloseBtn = document.querySelector('[data-modal-close]');
     modalCloseBtn.addEventListener('click', modalClose);
   }
+  modaltrailerContainer.classList.remove('is-hidden');
+  modaltrailerContainer.addEventListener('click', onBackdropClick);
 }
 
 export { openModalTrailer };
