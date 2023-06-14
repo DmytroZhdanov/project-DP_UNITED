fe3a4f0c4bbc49bf2a93fd583f50801bf9c5fcdd
Denys Cherednychenko <cherydens@ukr.net>
2023-06-14 13:55:31 +0300

    Continue clearing

--- src/js/functions/heroSlider.js
+++ src/js/functions/heroSlider.js
@@ -1,3 +1,6 @@
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 const heroSlider = {
   options: {
     prevArrow: '<button type="button" class="hero-slick-prev">&xlarr;</button>',
--- src/js/hero.js
+++ src/js/hero.js
@@ -5,9 +5,6 @@ import { openModalMovieDetails } from './functions/openModalMovieDetails';
 import { openModalTrailer } from './functions/openModalTrailer';
 import { heroSlider } from './functions/heroSlider';
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
 const refs = {
   heroSectionEl: document.querySelector('[data-hero-section]'),
   defaultHeroSectionEl: document.querySelector('[data-default-hero-section]'),
@@ -17,9 +14,7 @@ onHeroLoad();
 
 async function onHeroLoad() {
   try {
-    const response = await fetchDayTrends(Math.floor(Math.random() * 4 + 1));
-    // const randomDayMovie = getRandomDayMovie(response.results.length);
-    // const heroFilm = response.results[randomDayMovie];
+    const response = await fetchDayTrends(Math.round(Math.random() * 4 + 1));
     refs.heroSectionEl.innerHTML = generateHeroMarkup(response.results);
     heroSlider.enable();
 
@@ -34,6 +29,7 @@ async function onHeroLoad() {
         heroSlider.pause();
       })
     );
+
     modalMovieDetailsBtn.forEach(el =>
       el.addEventListener('click', () => {
         openModalMovieDetails(el.getAttribute('data-id'));
@@ -45,7 +41,3 @@ async function onHeroLoad() {
     refs.defaultHeroSectionEl.classList.remove('is-hidden');
   }
 }
-
-// function getRandomDayMovie(arrLength) {
-//   return Math.round(Math.random() * (arrLength - 1));
-// }
