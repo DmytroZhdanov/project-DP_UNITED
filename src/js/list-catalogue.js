5b672d770ba7c331e459e0c2cfc5543c32fcc239
Tanyusha7 <swanonlake@gmail.com>
2023-06-14 10:27:05 +0200

    update catalogue js, html, css

--- src/js/list-catalogue.js
+++ src/js/list-catalogue.js
@@ -1,71 +1,70 @@
 // Catalogue Page - Section Catalogue list
 import { fetchWeekTrends } from './functions/movieApiService';
 import { generateMovieCardsMarkup } from './functions/generateMovieCardsMarkup';
-// import { fetchGenresList } from './functions/movieApiService';
 import { fetchMovieBySearchQuery } from './functions/movieApiService';
+import { fetchMovieBySearchQueryAndYear } from './functions/movieApiService';
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 
 const catalogEl = document.querySelector('[data-catalogue-movies]');
-// console.log(catalogEl);
 const paginEl = document.querySelector('#tui-pagination-container');
-// console.log(paginEl);
 const catalogFormSearch = document.querySelector(
   '[data-catalogue-search-form]'
 );
-// console.log(catalogFormSearch);
+const choosefilm = document.querySelector('[data-catalogue-choose-movie]');
+const chooseYear = document.querySelector('[data-catalogue-choose-year]');
 const inputFilm = document.querySelector('#input-catalogue-film');
-// console.log(inputFilm);
 const inputYear = document.querySelector('#input-catalogue-year');
-// console.log(inputYear);
 const inputSearchByName = document.querySelector(
   '#input-catalogue-search-by-name'
 );
-// console.log(inputSearchByName);
-const iconClose = document.querySelector('#btn-input-catalogue-film');
-// console.log(iconClose);
-
+const iconClearFilm = document.querySelector('#btn-input-catalogue-film');
+const iconClearSearch = document.querySelector('#btn-input-catalogue-search');
 catalogFormSearch.addEventListener('submit', onCatalogSearchMovies);
-
+iconClearSearch.addEventListener('click', onClearInputSearch);
+iconClearFilm.addEventListener('click', onClearInputFilm);
+chooseYear.addEventListener('change', searchMoviesByYear);
+const moviesNumber = window.innerWidth < 768 ? 10 : 20;
+let searchQuery;
+let yearsArr = [];
 const options = {
   totalItems: 0,
-  itemsPerPage: 10,
+  itemsPerPage: 20,
   visiblePages: 4,
   page: 1,
 };
 
 const pagination = new Pagination(paginEl, options);
-const itemCount = 10;
 
 const page = pagination.getCurrentPage();
 
 pagination.on('afterMove', createPopularMoviesForWeek);
+const tuiPaginatBtn = document.querySelectorAll('.tui-page-btn');
+console.log(tuiPaginatBtn);
+// TRENDING MOVIES FOR WEEK //
 
 async function onRenderCatalogPage(page) {
   try {
     const resp = await fetchWeekTrends(page);
+    const moviesToRender = resp.results.slice(0, moviesNumber);
+    catalogEl.innerHTML = generateMovieCardsMarkup(moviesToRender);
 
-    catalogEl.innerHTML = generateMovieCardsMarkup(resp.results);
-    const tuiPaginatBtnSelected = document.querySelector('.tui-is-selected');
-    tuiPaginatBtnSelected.classList.add('btn-current');
     pagination.reset(resp.total_pages);
-    const tuiPaginatBtn = document.querySelectorAll('a.tui-page-btn');
+    const tuiPaginatBtn = document.querySelectorAll('.tui-page-btn');
+    tuiPaginatBtn.forEach(button => {
+      console.log(button);
+      button.classList.add('btn-number');
+    });
 
-    console.log(tuiPaginatBtnSelected);
     const tuiPaginatContainer = document.querySelector('.tui-pagination');
-    console.log(tuiPaginatBtn);
-    console.log(tuiPaginatBtn.length);
-    console.log(tuiPaginatContainer);
-    tuiPaginatContainer.classList.add('dark-theme', 'light-theme');
-
-    if (tuiPaginatBtnSelected) {
-    }
-    // tuiPaginatBtn.forEach(button => {
-    //   console.log(button);
-    //   button.classList.add('btn-number', 'dark-theme', 'light-theme');
-    // });
-
-    // pagination.setItemsPerPage(itemCount);
+    tuiPaginatContainer.classList.add(
+      'dark-theme',
+      'light-theme',
+      'btn-container'
+    );
+    const tuiPaginatBtnSelected = document.querySelector('.tui-is-selected');
+    console.log(tuiPaginatBtnSelected);
+    tuiPaginatBtnSelected.classList.add('btn-current');
 
     paginEl.classList.remove('is-hidden');
 
@@ -90,10 +89,17 @@ async function createPopularMoviesForWeek(e) {
 
   try {
     const resp = await fetchWeekTrends(currentPage);
+    const moviesToRender = resp.results.slice(0, moviesNumber);
+    catalogEl.innerHTML = generateMovieCardsMarkup(moviesToRender);
+    const tuiPaginatBtnSelected = document.querySelector('.tui-is-selected');
+    console.log(tuiPaginatBtnSelected);
+    tuiPaginatBtnSelected.classList.add('btn-current');
 
-    catalogEl.innerHTML = generateMovieCardsMarkup(resp.results);
-
-    pagination.setItemsPerPage(10);
+    const tuiPaginatBtn = document.querySelectorAll('.tui-page-btn');
+    tuiPaginatBtn.forEach(button => {
+      console.log(button);
+      button.classList.add('btn-number');
+    });
   } catch (err) {
     console.log(err);
   }
@@ -101,10 +107,12 @@ async function createPopularMoviesForWeek(e) {
 
 onRenderCatalogPage();
 
+// SEARCH MOVIES BY SEARCHQUERY //
+
 async function onCatalogSearchMovies(e) {
   e.preventDefault();
-  const searchQuery = e.currentTarget.elements['searchQuery'].value.trim();
-  console.log(searchQuery);
+  searchQuery = e.currentTarget.elements['searchQuery'].value.trim();
+
   if (!searchQuery) {
     console.error("Search querry can't be empty");
     return;
@@ -114,14 +122,21 @@ async function onCatalogSearchMovies(e) {
 
   try {
     const resp = await fetchMovieBySearchQuery(searchQuery, page);
-    console.log(resp);
-    catalogEl.innerHTML = generateMovieCardsMarkup(resp.results);
+
+    const moviesToRender = resp.results.slice(0, moviesNumber);
+
+    catalogEl.innerHTML = generateMovieCardsMarkup(moviesToRender);
+
+    iconClearSearch.hidden = false;
     inputFilm.hidden = false;
+    iconClearFilm.hidden = false;
     inputYear.hidden = false;
     pagination.reset(resp.total_pages);
 
     paginEl.classList.remove('is-hidden');
-    // catalogEl.innerHTML = generateMovieCardsMarkup(resp.results);
+    catalogEl.innerHTML = generateMovieCardsMarkup(moviesToRender);
+    choosefilm.innerHTML = markUpListOfMovies(moviesToRender);
+    // chooseYear.innerHTML = markUpListOfYears(resp.results);
 
     if (resp.total_results === 0) {
       catalogEl.innerHTML =
@@ -136,7 +151,6 @@ async function onCatalogSearchMovies(e) {
     }
 
     pagination.on('afterMove', createPaginationByQuerry);
-    // pagination.setItemsPerPage(10);
   } catch (err) {
     console.log(err);
   }
@@ -147,9 +161,67 @@ async function createPaginationByQuerry(evt) {
 
   try {
     const resp = await fetchMovieBySearchQuery(currentPage);
-    // pagination.setItemsPerPage(10);
+    const moviesToRender = resp.results.slice(0, moviesNumber);
+    catalogEl.innerHTML = generateMovieCardsMarkup(moviesToRender);
+    choosefilm.innerHTML = markUpListOfMovies(moviesToRender);
+    // chooseYear.innerHTML = markUpListOfYears(resp.results);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// SEARCH BY NAME AND YEAR ///
+
+async function searchMoviesByYear(e) {
+  year = e.currentTarget.value;
+  console.log(year);
+
+  try {
+    const resp = await fetchMovieBySearchQueryAndYear(year, searchQuery);
+
+    console.log(resp.results.release_date);
+  } catch (err) {
+    console.log(err);
+  }
+}
+async function createPaginationBySearchQueryAndYear(evt) {
+  const currentPage = evt.page;
+
+  try {
+    const resp = await fetchMovieBySearchQueryAndYear(currentPage);
+    console.log(resp);
     catalogEl.innerHTML = generateMovieCardsMarkup(resp.results);
   } catch (err) {
     console.log(err);
   }
+}
+
+// CLEAR INPUTS SEARCH NAME & FILMS //
+function onClearInputSearch() {
+  inputSearchByName.firstElementChild.value = '';
+  iconClearSearch.hidden = true;
+}
+
+function onClearInputFilm() {
+  inputFilm.firstElementChild.value = '';
+
+  iconClearFilm.hidden = true;
+}
+
+function markUpListOfMovies(arr) {
+  return arr
+    .map(({ title }) => `<option value='${title}'>${title}</option>`)
+    .join();
+}
+
+function markUpListOfYears(years) {
+  return years
+    .map(({ year }) => {
+      years = release_date.slice(0, 4);
+      console.log(years);
+      yearsArr.push(years);
+      console.log(year);
+      `<option value='${year}'>${year}</option>`;
+    })
+    .join();
 }
