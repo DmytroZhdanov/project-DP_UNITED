df44364d01d846473001b1ae4e6f31a6640957aa
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-06-14 19:41:07 +0200

    Ammends function to generate movie cards

--- src/js/functions/generateMovieCardsMarkup.js
+++ src/js/functions/generateMovieCardsMarkup.js
@@ -8,13 +8,15 @@ import { getGenresByGenresId } from './getGenresByGenresId';
 function generateMovieCardsMarkup(arr) {
   const markup = arr
     .map(
-      ({ genre_ids, id, poster_path, release_date, title, vote_average }) => {
+      ({ genre_ids, id, poster_path, release_date, title, vote_average, genres }) => {
         const year = release_date.slice(0, 4);
         const posterPath = poster_path
           ? `https://image.tmdb.org/t/p/w500${poster_path}`
           : './images/image-not-found.jpg';
         const ratingStarsMurkup = generateRatingStarsMarkup(vote_average);
-        const genres = getGenresByGenresId(genre_ids).join(', ');
+        const genresString = genre_ids
+          ? getGenresByGenresId(genre_ids).join(', ')
+          : genres.map(genre => genre.name).join(', ');
         return `<li class="cards-item">
                   <button type="button" class="btn cards-item-btn">
                     <img
@@ -25,7 +27,7 @@ function generateMovieCardsMarkup(arr) {
                     <div class="cards-content" id="${id}">
                       <div class="cards-text-content">
                         <h3 class="cards-title">${title}</h3>
-                        <p class="cards-text">${genres} | ${year}</p>
+                        <p class="cards-text">${genresString} | ${year}</p>
                       </div>
                       <div class="rating-stars-wrapper">${ratingStarsMurkup}
                       </div>
