2575cde039801f1368ae96f4a568d6f08db8a4c7
Dmytro Zhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-07-14 13:50:13 +0200

    Update libraryPageRendToGanre.js

--- src/js/functions/libraryPageRendToGanre.js
+++ src/js/functions/libraryPageRendToGanre.js
@@ -4,25 +4,16 @@ import { libraryRenderButtonLoadMore } from './libraryRenderButtonLoadMore';
 
 import { refs } from '../list-library';
 
-//нахожу кнопку лоад море для подальшего ее удаления при  рендере жанров
-
-//=================================конец===============================
-
 function libraryPageRendToGanre(
   totalFiltretPageOnLibrary,
   FilterGanreCurentPage,
   arrayFilteredMoviesToRender
 ) {
-  //  console.log('totalLibraryPage:', totalFiltretPageOnLibrary);
-  //  libraryRenderButtonLoadMore();
   libraryLoadMoreBtn = document.querySelector('[data-load-more-btn]');
-  console.log(libraryLoadMoreBtn);
   if (totalFiltretPageOnLibrary <= 1) {
     refs.libraryMovieList.innerHTML = generateMovieCardsMarkup(
       arrayFilteredMoviesToRender
     );
-    //refs.libraryMovieList.classList.add('movie-cards-list-css');
-    //refs.libraryMovieList.classList.add('movie-cards-list-css-no-btn');
     if (libraryLoadMoreBtn) {
       libraryLoadMoreBtn.remove();
     }
@@ -35,10 +26,8 @@ function libraryPageRendToGanre(
         FilterGanreCurentPage
       )
     );
-    console.log('libraryLoadMoreBtn:', libraryLoadMoreBtn);
     if (!libraryLoadMoreBtn) {
       libraryRenderButtonLoadMore();
-      console.log('if-libraryLoadMoreBtn:', !libraryLoadMoreBtn);
       FilterGanreCurentPage += 1;
 
       refs.libraryMovieList.classList.add('movie-cards-list-css');
@@ -49,15 +38,10 @@ function libraryPageRendToGanre(
         libraryRenderLoadMoreCardToFilterGanres
       );
     }
-    //  totalFiltretPageOnLibrary, FilterGanreCurentPage, arrayFilteredMoviesToRender
   }
   function libraryRenderLoadMoreCardToFilterGanres() {
-    console.log('total:', totalFiltretPageOnLibrary);
-    console.log('curent:', FilterGanreCurentPage);
-    console.log('array:', arrayFilteredMoviesToRender);
     //==================read div of button to delete div if no btn for css================================
     libraryDivLoadMoreBtn = document.querySelector('[data-div-load-more-btn]');
-    console.log('libraryDivLoadMoreBtn:', libraryDivLoadMoreBtn);
     // =================if the last page is left - render and hide the button============
     if (totalFiltretPageOnLibrary === FilterGanreCurentPage + 1) {
       refs.libraryMovieList.insertAdjacentHTML(
