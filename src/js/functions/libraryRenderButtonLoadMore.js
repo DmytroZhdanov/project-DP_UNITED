ae687b1f5f7e69ea3c1ee18d2e3c3ae1826446cc
Dmytro Zhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-07-14 14:11:13 +0200

    Update libraryRenderButtonLoadMore.js

--- src/js/functions/libraryRenderButtonLoadMore.js
+++ src/js/functions/libraryRenderButtonLoadMore.js
@@ -3,7 +3,7 @@ function libraryRenderButtonLoadMore() {
   refs.libraryMovieList.insertAdjacentHTML(
     'afterend',
     `
-                        <div class="btn-load-more" data-div-load-more-btn>
+                        <div class="btn-list-load-more" data-div-load-more-btn>
                             <button type="button" class="btn btn-filled" data-load-more-btn>
                             Load more
                             </button>
