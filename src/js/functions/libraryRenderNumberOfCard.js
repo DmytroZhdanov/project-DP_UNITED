9257517b0b3da38a124b2085a58edd7d8168d03e
Dmytro Zhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-07-14 14:13:15 +0200

    Update libraryRenderNumberOfCard.js

--- src/js/functions/libraryRenderNumberOfCard.js
+++ src/js/functions/libraryRenderNumberOfCard.js
@@ -1,4 +1,3 @@
-//import { refs } from '../list-library';
 //====================make arrayCard for every page=================================
 function libraryRenderNumberOfCard(array, n) {
   return array.slice(9 * n, 9 * n + 9);
